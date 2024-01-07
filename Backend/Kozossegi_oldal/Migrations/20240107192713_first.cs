fe953d3e8bc18477e0e6d51e6febd49b9d28fb9e
B-Gabor20 <gaborbodnar92@gmail.com>
2024-01-08 00:00:25 +0100

    5

---
+++ Backend/Kozossegi_oldal/Migrations/20240107192713_first.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kozossegi_oldal.Migrations
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Posts",
+                type: "varchar(30)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Posts");
+        }
+    }
+}
---
+++ komplexfeladat2/src/pages/PostsSingleElement.js
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+export function postSingleElement() {
+    const param = useParams();
+    const id = param.PostId;
+    const [post, setPost] = useState([]);
+    const [isFetchPending, setFetchPending] = useState(false);
+
+    useEffect(() => {
+        const fetchItem = async () => {
+          try {
+            const response = await fetch(`/api/items/${id}`);
+            if (!response.ok) {
+              throw new Error('Failed to fetch item');
+            }
+    
+            const data = await response.json();
+            setItem(data);
+          } catch (error) {
+            console.error('Error fetching item:', error);
+          }
+        };
+    
+        fetchItem();
+      }, [id]);
+    
+
+    return (
+    <div>
+      {isFetchPending || !posts.id ? (<div className='spinner-border'></div>) : (
+        <div>
+            <div>
+                <div className='postcard card col-xs-12 col-md-4 col-xl-2 d-inline-block m-1 p-2 text-center'>
+                    <p className='posttext'>{post.author}</p>
+                    <p className='posttext'>{post.category}</p>
+                    <img className="img-fluid img-responsive card-image" src={post.image} alt={post.title} />
+                    <p className='posttext'>{post.title}</p>
+                    <p className='posttext'>{post.content}</p>
+                    <p className='posttext'>Feltöltés dátuma: <br></br>{post.createdTime}</p>
+                    <NavLink to={`/`}>
+                        <button className="bi bi-backspace">Vissza</button>
+                    </NavLink>
+                </div>
+            </div>
+        </div>
+      )}
+    </div>
+    );
+}
+
+/*
+useEffect(() => {
+        setFetchPending(true);
+        (async() => {
+            try{
+            const response = await fetch(`http://localhost:5144/api/Post/id?Id=01a504cd-187c-49e3-8b67-b5dd45359822`);
+            const post2 = await response.json();
+            setPost(post2);
+        } 
+        catch(error){
+            console.log(error);
+        }
+        finally{
+            setFetchPending(false);
+        }
+        })();
+    },[id]);
+*/ 

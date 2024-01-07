a99a05ddf6171e9202c2b629ee11d1e3752affa6
B-Gabor20 <gaborbodnar92@gmail.com>
2024-01-07 23:54:49 +0100

    4

--- komplexfeladat2/src/pages/Newpost.js
+++ komplexfeladat2/src/pages/Newpost.js
@@ -11,21 +11,33 @@ import {
 const Newpost = () => {
   return (
     <div>
-      <div className='postcard card form-group row pb-3 col-xs-12 col-md-4 col-xl-2 m-1 p-2 text-center'>
-        <p className='posttext'>Author</p>
-        <input></input>
-        <p className='posttext'>Kategória</p>
-        <input></input>
-        <p className='posttext'>Cím</p>
-        <input></input>
-        <p className='posttext'>Leírás</p>
-        <input></input>
-        <p className='posttext'>Képurl</p>
-        <input></input>
-        <br></br>
-        <input type="submit" value="Beküldés" className="d-inline"></input>
+      <div className='postcard card form-group row col-xs-12 col-md-6 col-xl-4 mt-5 p-2 text-center'>
+        <div class="input-group mt-3 mb-3">
+          <input type="text" class="form-control mt-3" placeholder="Author" aria-label="Username" aria-describedby="basic-addon1"/>
+          </div>
+          <input type="text" class="form-control mt-2" placeholder="Title" aria-label="Username" aria-describedby="basic-addon1"/>
+          <input type="text" class="form-control mt-2" placeholder="Description" aria-label="Username" aria-describedby="basic-addon1"/>
+          <div class="input-group mb-3">
+          <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle mt-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Category
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Sport</a></li>
+          <li><a class="dropdown-item" href="#">Animal</a></li>
+          <li><a class="dropdown-item" href="#">Nature</a></li>
+          <li><a class="dropdown-item" href="#">Portrait</a></li>
+          <li><a class="dropdown-item" href="#">B@W</a></li>
+          <li><a class="dropdown-item" href="#">Art</a></li>
+          <li><a class="dropdown-item" href="#">Travel</a></li>
+          <li><a class="dropdown-item" href="#">Macro</a></li>
+          <li><a class="dropdown-item" href="#">Food</a></li>
+        </ul>
+        </div>
+      </div>
+      <input type="submit" value="Beküldés" className="btn btn-primary d-inline mb-2"></input>
         <NavLink to={`/`}>
-        <button className="d-inline">Vissza</button>
+          <button className="d-inline btn btn-secondary">Vissza</button>
         </NavLink>
       </div>
     </div>

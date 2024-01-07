4320eb9b5265c24754e50e33f00945a649e4c096
B-Gabor20 <gaborbodnar92@gmail.com>
2024-01-07 23:10:26 +0100

    3

--- komplexfeladat2/src/pages/Newpost.js
+++ komplexfeladat2/src/pages/Newpost.js
@@ -10,7 +10,25 @@ import {
 
 const Newpost = () => {
   return (
-    <div></div>
+    <div>
+      <div className='postcard card form-group row pb-3 col-xs-12 col-md-4 col-xl-2 m-1 p-2 text-center'>
+        <p className='posttext'>Author</p>
+        <input></input>
+        <p className='posttext'>Kategória</p>
+        <input></input>
+        <p className='posttext'>Cím</p>
+        <input></input>
+        <p className='posttext'>Leírás</p>
+        <input></input>
+        <p className='posttext'>Képurl</p>
+        <input></input>
+        <br></br>
+        <input type="submit" value="Beküldés" className="d-inline"></input>
+        <NavLink to={`/`}>
+        <button className="d-inline">Vissza</button>
+        </NavLink>
+      </div>
+    </div>
   );
 };
 

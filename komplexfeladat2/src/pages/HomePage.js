236842ccc9a408a8c02dd1ec3fbc82f0bcb7b525
B-Gabor20 <gaborbodnar92@gmail.com>
2024-01-07 19:43:43 +0100

    smoll update

--- komplexfeladat2/src/pages/HomePage.js
+++ komplexfeladat2/src/pages/HomePage.js
@@ -37,36 +37,3 @@ const HomePage = () => {
 };
 
 export default HomePage;
-/*
-<div className="homepage">
-      <div className="postcategoryarrow">
-        <div className="category">
-          <div className="morebutton">
-            <div className="morebuttonrectangle" />
-            <img className="moreicon" alt="" src="/moreicon@2x.png" />
-            <div className="moretext">Discover more</div>
-          </div>
-          <div className="post">
-            <img className="postimage-icon" alt="" src="/postimage@2x.png" />
-            <div className="postimageoverlay" />
-            <div className="postcontentbox" />
-            <div className="posttitle">Post title</div>
-            <div className="postcontent">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut...
-            </div>
-            <div className="postcontentoverlay" />
-            <div className="profiledname">Display name</div>
-          </div>
-          
-          <div className="categorytitle">
-            <div className="categorytitle1">CategoryName</div>
-            <img className="categoryicon" alt="" src="/categoryicon@2x.png" />
-          </div>
-        </div>
-      </div>
-    </div>
-
-*/

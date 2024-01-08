9c95b1384801b9d6e26bc1b849a14f3a68cbf287
B-Gabor20 <gaborbodnar92@gmail.com>
2024-01-08 09:09:58 +0100

    7

--- komplexfeladat2/src/index.js
+++ komplexfeladat2/src/index.js
@@ -1,20 +1,3 @@
-/*import React from "react";
-import ReactDOM from 'react-dom/client';
-import { createRoot } from "react-dom/client";
-import App from "./App";
-
-import { BrowserRouter } from "react-router-dom";
-import "./global.css";
-
-const container = document.getElementById("root");
-//const root = createRoot(container);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);*/
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./css/index.css";
